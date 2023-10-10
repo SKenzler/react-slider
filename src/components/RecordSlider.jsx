12d59e4172456a3cfa1e1fe2323173d76ae3d0cb
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-10-10 22:42:11 +0800

    Add a container to wrap the slider and add next plus previous buttons

--- src/components/RecordSlider.jsx
+++ src/components/RecordSlider.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { useState } from "react";
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import "../App.css";
 
 const RecordSlider = ({ imagePath }) => {
   const [imageIndex, setImageIndex] = useState(0);
   return (
-    <div>
-      <img src={imagePath[imageIndex]} />
-      <button type="button" className="control-btn">
-        <BsFillArrowLeftCircleFill />
+    <div className="container">
+      <img src={imagePath[imageIndex]} className="hero-image" />
+      <button type="button" className="control-btn left">
+        <BsFillArrowLeftCircleFill size={22} />
       </button>
-      <button type="button" className="control-btn">
-        <BsFillArrowRightCircleFill />
+      <button type="button" className="control-btn right">
+        <BsFillArrowRightCircleFill size={22} />
       </button>
     </div>
   );
