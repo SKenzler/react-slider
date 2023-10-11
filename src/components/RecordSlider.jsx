4b52df24ff8a78e178e21bd992ed85148a05861b
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-10-11 21:58:04 +0800

    update css

--- src/components/RecordSlider.jsx
+++ src/components/RecordSlider.jsx
@@ -6,13 +6,35 @@ import "../App.css";
 
 const RecordSlider = ({ imagePath }) => {
   const [imageIndex, setImageIndex] = useState(0);
+
+  const displayNextSlide = () => {
+    setImageIndex((index) => {
+      if (index === imagePath.length - 1) return 0;
+      return index + 1;
+    });
+  };
+  const displayPrevSlide = () => {
+    setImageIndex((index) => {
+      if (index === 0) return imagePath.length - 1;
+      return index - 1;
+    });
+  };
+
   return (
     <div className="container">
       <img src={imagePath[imageIndex]} className="hero-image" />
-      <button type="button" className="control-btn left">
+      <button
+        type="button"
+        className="control-btn left"
+        onClick={displayPrevSlide}
+      >
         <BsFillArrowLeftCircleFill size={22} />
       </button>
-      <button type="button" className="control-btn right">
+      <button
+        type="button"
+        className="control-btn right"
+        onClick={displayNextSlide}
+      >
         <BsFillArrowRightCircleFill size={22} />
       </button>
     </div>
